992bdf39f888afe1ab511c0787890fb865173571
Dan Dumitrache <saigon@ddnro.Home>
2018-05-03 11:15:59 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PostCodesUK
+
+Simple Python program that formats and validates post codes for UK.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='postcodesuk',
+	version='0.0.1',
+	description='a pip-installable package that formats and validates post codes for UK',
+	packages=['postcodesuk'],
+	author='Dan Dumitrache',
+	author_email='vietnamezul@gmail.com',
+	license='GNU General Public License',
+	python_requires='~=3.3',
+)
